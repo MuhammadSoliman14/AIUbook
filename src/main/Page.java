3375b2f20c1d33023f5be28e75bd0e26c09d74f9
MuhammadSoliman14 <84283979+MuhammadSoliman14@users.noreply.github.com>
2021-05-27 23:36:17 +0200

    Update Page.java

--- src/main/Page.java
+++ src/main/Page.java
@@ -1,5 +1,12 @@
-package main;
+	public class Pages extends Groups{
 
-public class Page {
+	public void create() {
 
-}
+		System.out.println("Enter your page name");
+		setName(name);
+		System.out.println("What is your page about?");
+		setAbout(about);
+		System.out.println("Set your page privacy");
+		setPrivacy(privacy);
+	}
+	}
